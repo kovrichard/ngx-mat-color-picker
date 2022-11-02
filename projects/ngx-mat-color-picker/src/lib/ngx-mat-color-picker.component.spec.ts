7aff4a4f28c099ef69aac9c321b6381a0154273a
Richard Kovacs <richardkovacs.it@gmail.com>
2022-11-02 20:20:21 +0100

    feat: add tests, scss and html

--- projects/ngx-mat-color-picker/src/lib/ngx-mat-color-picker.component.spec.ts
+++ projects/ngx-mat-color-picker/src/lib/ngx-mat-color-picker.component.spec.ts
@@ -1,22 +1,94 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgxMatColorPickerComponent } from './ngx-mat-color-picker.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { FormControl, FormGroup } from '@angular/forms';
 
 describe('NgxMatColorPickerComponent', () => {
   let component: NgxMatColorPickerComponent;
   let fixture: ComponentFixture<NgxMatColorPickerComponent>;
+  let form: FormGroup;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [OverlayModule],
       declarations: [NgxMatColorPickerComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NgxMatColorPickerComponent);
     component = fixture.componentInstance;
+
+    form = new FormGroup({
+      color: new FormControl('#000000'),
+    });
+    component.formControlName = 'color';
+    component.form = form;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('default color should be white', () => {
+    expect(component.color).toEqual('#FFFFFF');
+  });
+
+  it('color picker should be closed by default', () => {
+    expect(component.pickerVisible).toBeFalse();
+  });
+
+  it('color should change when input color changes', () => {
+    component.changeFormColor('#abcabc');
+
+    expect(component.color).toEqual('#ABCABC');
+  });
+
+  it('color should not change when input color is invalid', () => {
+    component.changeFormColor('#xxyyzz');
+
+    expect(component.color).toEqual('#FFFFFF');
+  });
+
+  it('onChange should be changeable', () => {
+    const a = () => {};
+    component.registerOnChange(a);
+
+    expect(component.onChange).toEqual(a);
+  });
+
+  it('onTouched should be changeable', () => {
+    const a = () => {};
+    component.registerOnTouched(a);
+
+    expect(component.onTouched).toEqual(a);
+  });
+
+  it('changeFormColor should change form control color based on name', () => {
+    const color = '#aabbcc';
+    component.changeFormColor(color);
+
+    expect(form.get(component.formControlName)?.value).toEqual(
+      color.toUpperCase()
+    );
+  });
+
+  it('onEscape should prevent default behavior', () => {
+    const mockEvent: any = {
+      preventDefault: () => {},
+    };
+    const preventMock = spyOn(mockEvent, 'preventDefault');
+
+    component.onEscape(mockEvent);
+
+    expect(preventMock).toHaveBeenCalledOnceWith();
+  });
+
+  it('onEscape should should close picker', () => {
+    const mockEvent: any = {
+      preventDefault: () => {},
+    };
+    component.pickerVisible = true;
+
+    component.onEscape(mockEvent);
+
+    expect(component.pickerVisible).toBeFalse();
   });
 });
--- projects/ngx-mat-color-picker/src/lib/ngx-mat-color-picker.component.ts
+++ projects/ngx-mat-color-picker/src/lib/ngx-mat-color-picker.component.ts
@@ -1,14 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  OnInit,
+  forwardRef,
+} from '@angular/core';
+import { MatFormFieldAppearance } from '@angular/material/form-field';
+import {
+  ControlValueAccessor,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { ConnectionPositionPair } from '@angular/cdk/overlay';
 
 @Component({
-  selector: 'lib-ngx-mat-color-picker',
-  template: ` <p>ngx-mat-color-picker works!</p> `,
-  styles: [],
+  selector: 'ngx-mat-color-picker',
+  templateUrl: './ngx-mat-color-picker.component.html',
+  styleUrls: ['./ngx-mat-color-picker.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NgxMatColorPickerComponent),
+      multi: true,
+    },
+  ],
 })
-export class NgxMatColorPickerComponent implements OnInit {
+export class NgxMatColorPickerComponent
+  implements OnInit, ControlValueAccessor
+{
+  @Input() appearance: MatFormFieldAppearance;
+  @Input() label: string;
+  @Input() placeholder: string;
+  @Input() formControlName: string;
+  @Input() form: FormGroup;
+
+  positionPairs: ConnectionPositionPair[] = [
+    {
+      originX: 'start',
+      originY: 'bottom',
+      overlayX: 'start',
+      overlayY: 'bottom',
+      offsetX: -210,
+      offsetY: 0,
+    },
+  ];
+
+  color = '#FFFFFF';
+  pickerVisible = false;
+
   constructor() {}
 
   ngOnInit(): void {
-    console.log();
+    this.form
+      .get(this.formControlName)
+      ?.valueChanges.subscribe((value: string) => {
+        if (this.validColor(value)) {
+          this.color = value.toUpperCase();
+        }
+      });
+  }
+
+  onChange: any = () => {};
+
+  onTouched: any = () => {};
+
+  writeValue(color: string) {
+    this.color = color;
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  changeFormColor($event: any) {
+    this.form.get(this.formControlName)?.setValue($event.toUpperCase());
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscape($event: KeyboardEvent) {
+    $event.preventDefault();
+
+    this.pickerVisible = false;
+  }
+
+  @HostListener('document:keydown.enter', ['$event'])
+  onEnter($event: KeyboardEvent) {
+    $event.preventDefault();
+  }
+
+  private validColor(color: string) {
+    return /^#[0-9a-fA-F]{6}$/i.test(color);
   }
 }
